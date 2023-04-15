5efcfecf5d8b394feddeff9998ecd0bdf756f010
Paul Zurita <paulzuri@tutanota.com>
2023-04-15 00:55:54 -0500

    second version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot("6245178048:AAFecsyw8XrPYbjAZsTzLfO7WMU5rw1lDEY")
 @bot.message_handler(commands=["help", "start"])
 
 def enviar(message):
-    bot.reply_to(message,"hola, soy TestBot")
+    bot.reply_to(message,"hola, soy ImitaBot, respondo con el mismo mensaje que tú envías, pruébame")
 
 @bot.message_handler(func=lambda message:True)
 
